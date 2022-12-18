f3bdb7dbbebb5b0f29506baf0cb470f44a4cec87
Vũ Đạt <datcy2011@gmail.com>
2022-12-18 22:54:57 +0700

    Add project files.

---
+++ 2001202045_VuNgoDat_SM24_BTVN_Buoi_11/frmNhapLop.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace _2001202045_VuNgoDat_SM24_BTVN_Buoi_11
+{
+    partial class frmNhapLop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnThem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnXoa = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSua = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLuu = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnXem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDong = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaLop = new System.Windows.Forms.TextBox();
+            this.txtTenLop = new System.Windows.Forms.TextBox();
+            this.cboKhoa = new System.Windows.Forms.ComboBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnThem,
+            this.btnXoa,
+            this.btnSua,
+            this.btnLuu,
+            this.btnXem,
+            this.btnDong});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(60, 24);
+            this.btnThem.Text = "Thêm";
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(49, 24);
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(48, 24);
+            this.btnSua.Text = "Sửa";
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(47, 24);
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXem
+            // 
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(69, 24);
+            this.btnXem.Text = "Xem in";
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(60, 24);
+            this.btnDong.Text = "Đóng";
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã lớp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Khoa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên lớp";
+            // 
+            // txtMaLop
+            // 
+            this.txtMaLop.Location = new System.Drawing.Point(86, 21);
+            this.txtMaLop.Name = "txtMaLop";
+            this.txtMaLop.Size = new System.Drawing.Size(214, 22);
+            this.txtMaLop.TabIndex = 4;
+            // 
+            // txtTenLop
+            // 
+            this.txtTenLop.Location = new System.Drawing.Point(401, 21);
+            this.txtTenLop.Name = "txtTenLop";
+            this.txtTenLop.Size = new System.Drawing.Size(214, 22);
+            this.txtTenLop.TabIndex = 5;
+            // 
+            // cboKhoa
+            // 
+            this.cboKhoa.FormattingEnabled = true;
+            this.cboKhoa.Location = new System.Drawing.Point(86, 66);
+            this.cboKhoa.Name = "cboKhoa";
+            this.cboKhoa.Size = new System.Drawing.Size(214, 24);
+            this.cboKhoa.TabIndex = 6;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 170);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(776, 268);
+            this.dgv.TabIndex = 7;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            this.dgv.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dgv_DataError);
+            // 
+            // pnl
+            // 
+            this.pnl.Controls.Add(this.cboKhoa);
+            this.pnl.Controls.Add(this.txtTenLop);
+            this.pnl.Controls.Add(this.txtMaLop);
+            this.pnl.Controls.Add(this.label3);
+            this.pnl.Controls.Add(this.label2);
+            this.pnl.Controls.Add(this.label1);
+            this.pnl.Location = new System.Drawing.Point(15, 57);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(633, 100);
+            this.pnl.TabIndex = 8;
+            // 
+            // frmNhapLop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnl);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmNhapLop";
+            this.Text = "Nhập Lớp";
+            this.Load += new System.EventHandler(this.frmNhapLop_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.pnl.ResumeLayout(false);
+            this.pnl.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnThem;
+        private System.Windows.Forms.ToolStripMenuItem btnXoa;
+        private System.Windows.Forms.ToolStripMenuItem btnSua;
+        private System.Windows.Forms.ToolStripMenuItem btnLuu;
+        private System.Windows.Forms.ToolStripMenuItem btnXem;
+        private System.Windows.Forms.ToolStripMenuItem btnDong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaLop;
+        private System.Windows.Forms.TextBox txtTenLop;
+        private System.Windows.Forms.ComboBox cboKhoa;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Panel pnl;
+    }
+}
+
---
+++ 2001202045_VuNgoDat_SM24_BTVN_Buoi_11/frmNhapLop.cs
@@ -0,0 +1,165 @@
+﻿using _2001202045_VuNgoDat_SM24_BTVN_Buoi_11.Utilites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2001202045_VuNgoDat_SM24_BTVN_Buoi_11
+{
+    public partial class frmNhapLop : Form
+    {
+        Core core;
+        DataSet ds_QLSV;
+        DataColumn[] keyKhoa = new DataColumn[1];
+        DataColumn[] keyLop = new DataColumn[1];
+        DataColumn[] keySV = new DataColumn[1];
+        public frmNhapLop()
+        {
+            ds_QLSV = new DataSet();
+            core = new Core();
+            InitializeComponent();
+        }
+
+        private void frmNhapLop_Load(object sender, EventArgs e)
+        {
+            dgv.MultiSelect = false;
+            //Load data
+            core.LoadDataIntoCbo(cboKhoa, ds_QLSV, "Select * from khoa", "khoa" ,"tenkhoa", "makhoa");
+            core.LoadDataIntoDgv(dgv, ds_QLSV, "select * from lop", "lop");
+            //Disable all textbox + cbo
+            pnl.Controls.OfType<Control>().ToList().ForEach(x =>
+            {
+                if (x.GetType() != typeof(Label))
+                {
+                    x.Enabled = false;
+                }
+            });
+            //Disable btn
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnLuu.Enabled = false;
+            //Fill data into SinhVien table
+            core.FillData(ds_QLSV, "select * from sinhvien", "sinhvien");
+            //Add primary keys for tables
+            keyKhoa[0] = ds_QLSV.Tables["KHOA"].Columns[0];
+            ds_QLSV.Tables["KHOA"].PrimaryKey = keyKhoa;
+
+            keyLop[0] = ds_QLSV.Tables["LOP"].Columns[0];
+            ds_QLSV.Tables["LOP"].PrimaryKey = keyLop;
+
+            keySV[0] = ds_QLSV.Tables["SINHVIEN"].Columns[0];
+            ds_QLSV.Tables["SINHVIEN"].PrimaryKey = keySV;
+
+        }
+        private void DataBindingToDgv(DataTable dt)
+        {
+            pnl.Controls.OfType<Control>().ToList().ForEach(x => {
+                x.DataBindings.Clear();
+            });
+            txtMaLop.DataBindings.Add("Text",dt, "MALOP");
+            txtTenLop.DataBindings.Add("Text", dt, "TENLOP");
+        }
+
+        #region Controls
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát","Hệ thống", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string selectCmd = "Select * from Lop";
+            DataRow newRow = ds_QLSV.Tables["LOP"].NewRow();
+            if (newRow != null)
+            {
+                if (core.Update(ds_QLSV, selectCmd, "Lop") == 0)
+                {
+                    MessageBox.Show("Cập nhật thất bại!", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Cập nhật thành công!", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cập nhật thất bại!", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btnLuu.Enabled = false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            txtMaLop.DataBindings.Clear();
+            btnLuu.Enabled = true;
+            dgv.AllowUserToAddRows = false;
+            dgv.ReadOnly = false;
+            dgv.Columns[0].ReadOnly = true;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn xóa?", "Hệ thống", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                foreach (DataRow rowS in ds_QLSV.Tables["SinhVien"].Rows)
+                {
+                    if (rowS["malop"].ToString().Equals(txtMaLop.Text.Trim()))
+                    {
+                        MessageBox.Show("Khóa đang được sử dụng ở bảng SinhVien!", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    
+                }
+                DataRow row = ds_QLSV.Tables["Lop"].Rows.Find(txtMaLop.Text.Trim());
+                if (row != null)
+                {
+                    row.Delete();
+                }
+                core.Update(ds_QLSV, "select * from lop", "lop");
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            txtMaLop.DataBindings.Clear();
+            btnLuu.Enabled = true;
+            dgv.AllowUserToAddRows = true;
+            dgv.ReadOnly = false;
+            for (int i = 0; i < dgv.Rows.Count - 1; i++)
+            {
+                dgv.Rows[i].ReadOnly = true;
+            }
+            dgv.FirstDisplayedScrollingColumnIndex = dgv.Rows.Count - 2;
+        }
+        #endregion
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            DataBindingToDgv(ds_QLSV.Tables["LOP"]);
+            DataGridViewRow row = dgv.CurrentRow;
+            cboKhoa.SelectedValue = row.Cells[2].Value.ToString();
+        }
+
+        private void dgv_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show("Đã tồn tại khóa!", "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+    }
+}
